5a01819b9d29f0af60fb4d0fa946b5661058413f
rr-ds <robson.rodrigues@hotmail.com>
2023-11-03 13:58:57 -0300

    5

--- lib/rest/boi_rest.dart
+++ lib/rest/boi_rest.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -5,6 +6,7 @@ import 'package:munick/model/boi.dart';
 import 'package:munick/rest/api.dart';
 
 class BoiRest {
+
   Future<Boi> buscar(int id) async {
     final http.Response response =
         await http.get(Uri.http(Api.endpoint, '/bois/$id'));
@@ -15,7 +17,7 @@ class BoiRest {
         'Erro buscando boi: ${id} [code: ${response.statusCode}]' );
     }
   }
-
+//BOI_REST.DART (3)
   Future<List<Boi>> buscarTodos() async {
     final http.Response response = 
       await http.get (Uri.http(API.endpoint, "bois"));
@@ -26,7 +28,51 @@ class BoiRest {
        }
 
   }
-  Future<Boi> inserir(Boi boi) async {}
-  Future<Boi> alterar(Boi boi) async {}
-  Future<Boi> remover(int id) async {}
+  //BOI_REST.DART (4)
+  Future<Boi> inserir(Boi boi) async {
+    final http.Response response = await http.post (
+      Uri.http(API.endpoint, 'bois'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: boi.toJson()
+    );
+  }
+
+//BOI_REST.DART (5)
+  Future<Boi> alterar(Boi boi) async {
+        final http.Response response = await http.put(
+          Uri.http(API.endpoint, 'bois/${boi.id}'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        body: boi.toJson(), 
+        );
+        if (response.statusCode == 200) {
+          return boi;
+        } else {
+          throw Exception ('Erro alternando boi ${boi.id}.');
+        }}
+      
+  
+//BOI_REST.DART (6)  
+  Future<Boi> remover(int id) async {
+      final http.Response response = 
+      await http.delete (
+        Uri.http (API.endpoint, '/bois/$id'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8' ,
+        }
+      );
+      if (response.statusCode == 200) {
+        return Boi.fromJson(response.body);
+        } else {
+          throw Exception('Erro removido boi: $id.');
+        }
+
+
+
+
+
+  }
 }
