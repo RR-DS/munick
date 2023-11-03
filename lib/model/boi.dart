a515001fae3f51d2c643e762666808db8aa718d6
rr-ds <robson.rodrigues@hotmail.com>
2023-11-03 10:30:02 -0300

    4

--- lib/model/boi.dart
+++ lib/model/boi.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/rendering.dart';
+
 class Boi {
   int? id;
   String nome;
@@ -6,4 +10,12 @@ class Boi {
 
   Boi(this.id, this.nome, this.raca, this.idade);
   Boi.novo(this.nome, this.raca, this.idade);
+
+  static Boi fromJson(String j) => Boi.fromMap(jsonDecode(j));
+  static List<Boi> fromJsonList(String j) {
+    final parsed = jsonDecode(j).cast<Map<String, dynamic>>();
+    return parsed.map<Boi>((map) => Boi.fromMap(map)).toList();
+  }
+
+  String toJson() => jsonEncode(toMap());
 }
