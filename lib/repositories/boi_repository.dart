2768e61dab8cdd78ba22ba69abe6c20e255c7063
rr-ds <robson.rodrigues@hotmail.com>
2023-11-05 14:30:53 -0300

    7

--- lib/repositories/boi_repository.dart
+++ lib/repositories/boi_repository.dart
@@ -1,3 +1,27 @@
-class BoiRepository {
+import 'dart:async';
 
+import 'package:munick/model/boi.dart';
+import 'package:munick/rest/boi_rest.dart';
+
+class BoiRepository {
+  final BoiRest api = BoiRest();
+  Future<Boi> buscar(int id) async {
+    return await api.buscar(id);
+  }
+
+  Future<List<Boi>> buscarTodos() async {
+    return await api.buscarTodos();
+  }
+
+  Future<Boi> inserir(Boi boi) async {
+    return await api.inserir(boi);
+  }
+
+  Future<Boi> alterar(Boi boi) async {
+    return await api.inserir(boi);
+  }
+
+  Future<Boi> remover(int id) async {
+    return await api.remover(id);
+  }
 }
