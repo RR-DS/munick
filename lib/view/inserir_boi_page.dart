2c796836261d342a0bdf6c7e5de146d9a1ad0a63
rr-ds <robson.rodrigues@hotmail.com>
2023-11-06 15:42:05 -0300

    10

--- lib/view/inserir_boi_page.dart
+++ lib/view/inserir_boi_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:munick/main.dart';
+import 'package:munick/model/boi.dart';
+import 'package:munick/repositories/boi_repository.dart';
 
 class InserirBoiPage extends StatefulWidget {
   static const String routeName = '/insert';
@@ -24,14 +26,34 @@ class _InserirBoiState extends State<InserirBoiPage> {
     super.dispose();
   }
 
-//SALVAR - INT COM BANCO
-  void _salvar() async {
+//SALVAR - INT COM BANCO - ANTIGO
+  /* void _salvar() async {
     _nomeController.clear();
     _racaController.clear();
     _idadeController.clear();
 
     ScaffoldMessenger.of(context)
         .showSnackBar(SnackBar(content: Text('Boi salvo com sucesso')));
+  }
+*/
+  //SALVAR - INT COM BANCO - NOVO
+
+  void _salvar() async {
+    Boi boi = Boi.novo(_nomeController.text, _racaController.text,
+        int.parse(_idadeController.text));
+
+    try {
+      BoiRepository repository = BoiRepository();
+      await repository.inserir(boi);
+      _nomeController.clear();
+      _racaController.clear();
+      _idadeController.clear();
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Boi salvo com sucesso')));
+    } catch (exception) {
+      showError(context, "Erro inserindo boi", exception.toString());
+    }
   }
 
 //BUILDFORM
