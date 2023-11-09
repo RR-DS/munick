3c81e7feb6106f22fca96499fd8222a5064ab273
rr-ds <robson.rodrigues@hotmail.com>
2023-11-09 09:55:20 -0300

    15

--- lib/model/boi.dart
+++ lib/model/boi.dart
@@ -10,14 +10,29 @@ class Boi {
 
   Boi(this.id, this.nome, this.raca, this.idade);
   Boi.novo(this.nome, this.raca, this.idade);
-  
+
   Map<String, dynamic> toMap() {
-    return{
+    return {
       'boi_id': this.id,
       'boi_nome': this.nome,
       'boi_raca': this.raca,
       'boi_idade': this.idade
-    }
+    };
+  }
+
+  static Boi fromMap(Map<String, dynamic> map) {
+    return Boi(
+      map['boi_id'],
+      map['boi_nome'],
+      map['boi_raca'],
+      map['boi_idade'],
+    ); //id, nome, raca, idade)
+  }
+
+  static List<Boi> fromMaps(List<Map<String, dynamic>> maps) {
+    return List.generate(maps.length, (i) {
+      return Boi.fromMap(maps[i]);
+    });
   }
 
   static Boi fromJson(String j) => Boi.fromMap(jsonDecode(j));
