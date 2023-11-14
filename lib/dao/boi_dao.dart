41d7e32817ef98d835de1b93c527b86334bb3110
rr-ds <robson.rodrigues@hotmail.com>
2023-11-14 11:31:54 -0300

    21

--- lib/dao/boi_dao.dart
+++ lib/dao/boi_dao.dart
@@ -7,56 +7,60 @@ class BoiDAO {
   Database database;
   BoiDAO(this.database);
 
-  Future<void> inserir(Boi boi) async {}
-  Future<List<Boi>> obterTodos() async {}
-  Future<Boi?> obterPorId(int id) async {}
-  Future<void> atualizar(Boi boi) async {}
-  Future<void> remover(int id) async {}
-}
-
-//INSERIR   PG 250
-Future<void> inserir(Boi boi) async {
-  await database.insert(
-    "tb_boi",
-    boi.toMap(),
-    conflictAlgorithm: ConflictAlgorithm.replace,
-  );
-}
-
-//OBTERTODOS_PG251
-Future<List<Boi>> obterTodos() async {
-  final List<Map<String, dynamic>> maps = await database.query("tb_boi");
-  return Boi.fromMaps(maps);
-}
-
-//OBTERPORID_PG251
-Future<Boi?> obterPorId(int id) async {
-  final List<Map<String, dynamic>> maps =
-      await database.query("tb_boi", where: "boi_id = ?", whereArgs: [id]);
-  if (maps.length > 0) {
-    return Boi.fromMap(maps.first);
+  //INSERIR   PG 250
+  Future<void> inserir(Boi boi) async {
+    await database.insert(
+      "tb_boi",
+      boi.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
-  return null;
-}
 
-//ATUALIZAR_PG252
-Future<void> atualizar(Boi boi) async {
-  await database.update(
-    'tb_boi',
-    boi.toMap(),
-    where: "boi_id = ?",
-    whereArgs: [boi.id],
-  );
-}
+  //OBTERTODOS_PG251
+  Future<List<Boi>> obterTodos() async {
+    final List<Map<String, dynamic>> maps = await database.query("tb_boi");
+    return Boi.fromMaps(maps);
+  }
+
+  //OBTERPORID_PG251
+  Future<Boi?> obterPorId(int id) async {
+    final List<Map<String, dynamic>> maps =
+        await database.query("tb_boi", where: "boi_id = ?", whereArgs: [id]);
+    if (maps.length > 0) {
+      return Boi.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  //ATUALIZAR_PG252
+  Future<void> atualizar(Boi boi) async {
+    await database.update(
+      'tb_boi',
+      boi.toMap(),
+      where: "boi_id = ?",
+      whereArgs: [boi.id],
+    );
+  }
 
 //REMOVER._PG252
-Future<void> remover(int id) async {
-  await database.delete(
-    'tb_boi',
-    where: "boi_id = ?",
-    whereArgs: [id],
-  );
+  Future<void> remover(int id) async {
+    await database.delete(
+      'tb_boi',
+      where: "boi_id = ?",
+      whereArgs: [id],
+    );
+  }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 /*
